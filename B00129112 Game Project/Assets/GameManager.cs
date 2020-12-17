14418bc3c2339f7299d99c8d9b6b5458a75edc95
DylanFitz2000 <B00129112@mytudublin.ie>
2020-12-17 19:52:55 +0000

    Add empty project

---
+++ B00129112 Game Project/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public MenuControls GameStartText;
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+   public void EndGame ()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER!");
+            Restart();
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
